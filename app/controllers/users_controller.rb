f270ecd471a4dd5b4bf129aa583dadd71acdcb9b
Christina Delpone <cdelpone@gmail.com>
2022-01-05 11:15:21 -0700

    Create user controller with dash route to user show

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,20 @@
+class UsersController < ApplicationController
+  def create
+    require "pry"; binding.pry
+
+    auth_hash = request.env['omniauth.auth']
+    google_id = auth_hash['uid']
+    email = auth_hash['info']['email']
+    token = auth_hash['credentails']['token']
+
+    user = User.find_or_create_by(email: email)
+    user.update(google_id: google_id)
+
+    session[:user_id] = user.id
+    redirect_to '/dashboard'
+  end
+  
+  def show
+    @user = User.find(session[:user_id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/auth/google_oauth2/callback', to: 'users#create'
+  get '/dashboard', to: 'users#show'
 end
