c5ca581e464ad89e847ca5cee84434f3d57cc6a7
Christina Delpone <cdelpone@gmail.com>
2022-01-07 10:05:43 -0700

    Refactor users update, show, sessions create

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,23 +1,24 @@
 class SessionsController < ApplicationController
   def create
-    # user = User.find_or_create_by(email: user_params[:email])
-    # user.update(google_id: user_params[:google_id], first_name: user_params[:first_name], last_name: user_params[:last_name])
-    user = BackendService.find_or_create_user(email: user_params[:email])
+    # user = BackendService.find_or_create_user(email: user_params[:email])
+    user = User.find_or_create_by(email: user_params[:email])
+    user.update(google_id: user_params[:google_id], first_name: user_params[:first_name], last_name: user_params[:last_name])
 
     if user.present?
       redirect_to '/dashboard'
     else
       flash[:error] = 'Authentication failed. Please try again.'
+      redirect_to root_path
     end
 
-    session[:google_token] = user_params[:token]
+    session[:token] = user_params[:token]
     session[:user_id] = user.id
 
-    unless user.street_address_1
-      redirect_to '/register'
-    else
-      redirect_to '/dashboard'
-    end
+    # unless user.street_address_1
+    #   redirect_to '/edit'
+    # else
+    #   redirect_to '/dashboard'
+    # end
   end
 
   def destroy
@@ -27,8 +28,11 @@ class SessionsController < ApplicationController
 
   private
 
+  def auth_hash
+    request.env['omniauth.auth']
+  end
+
   def user_params
-    auth_hash = request.env['omniauth.auth']
     {
       google_id: auth_hash['uid'],
       email: auth_hash['info']['email'],
