f2a6c7489f641bcaff4493f4d859faa68395212a
Christina Delpone <cdelpone@gmail.com>
2022-01-09 18:03:42 -0700

    Update facade to create user and rep objects

--- app/facades/user_dashboard_facade.rb
+++ app/facades/user_dashboard_facade.rb
@@ -1,9 +1,17 @@
 class UserDashboardFacade
-  attr_reader :representatives,
-              :user
 
   def initialize(user_id)
-    @representatives = BackendService.representatives(user_id)
     @user = BackendService.get_user(user_id)
+    @representatives = BackendService.representatives(user_id)
+  end
+
+  def user
+    UserBe.new(@user)
+  end
+
+  def representatives
+    @representatives.map do |rep|
+      Representative.new(rep)
+    end
   end
 end
