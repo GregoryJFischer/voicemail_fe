3f1e163d98ec2fbc474e0f2618499d75bf7ceb8d
Christina Delpone <cdelpone@gmail.com>
2022-01-09 18:00:51 -0700

    Update service with be endpoints

--- app/services/backend_service.rb
+++ app/services/backend_service.rb
@@ -1,30 +1,30 @@
 class BackendService
   class << self
     def get_user(user_id)
-      fetch("/users/#{user_id}")
+      fetch("users/#{user_id}")
     end
 
     def find_or_create_user(user_params)
-      post("/users", user_params)
+      parse_response(post("users", user_params))
     end
 
     def update_address(address_params, user_id)
-      patch("/users/#{user_id}", address_params)
+      patch("users/#{user_id}", address_params)
     end
 
     def representatives(user_id)
-      fetch("/users/#{user_id}/representatives")
+      fetch("users/#{user_id}/representatives")
     end
 
     def fetch(url)
-      parse_response(conn.get(url))
+      parse_response(conn.get("/api/v1/#{url}"))
     end
 
     def post(url, json)
       conn.post do |req|
         req.url "/api/v1/#{url}"
         req.headers['Content-Type'] = 'application/json'
-        req.body = json
+        req.body = json.to_json
       end
     end
 
