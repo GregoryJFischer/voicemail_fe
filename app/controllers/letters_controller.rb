5261a9aaaac9cd9e66d1398af3026af3ef30c73f
Billy Frey <bfrey16@gmail.com>
2022-01-10 13:32:35 -0700

    feat: letters controller first draft - untested

--- app/controllers/letters_controller.rb
+++ app/controllers/letters_controller.rb
@@ -2,6 +2,42 @@ class LettersController < ApplicationController
   def new
   end
   def create
-
+     post "/api/v1/letters", params: user_params
+     confirmation = JSON.parse(response.body, symbolize_names: true)
+     if confirmation[:data][:attributes].has_key?(:send_date)
+       @sent_letter_params = confirmation[:date][:attributes]
+     elsif confirmation.has_key?(:message)
+       flash[:error] = confirmation[:message]
+     else
+       flash[:error] = 'Error and error message not found'
   end
+
+
+  private
+    def user_params
+      user_id = session[:user_id]
+      get "api/v1/users/#{user_id}"
+      user_body = JSON.parse(response.body, symbolize_names: true)
+      user_attributes = user_body[:data][:attributes]
+
+      get "/api/v1/users/#{user.id}/representatives"
+      rep_body = JSON.parse(response.body, symbolize_names: true)
+      rep_attributes = rep_body[:data][:attributes]
+      {
+        to_address_line1: rep_attributes[:address_line1],
+        to_address_line2: rep_attributes[:address_line2],
+        to_address_city: rep_attributes[:address_city],
+        to_address_state: rep_attributes[:address_state],
+        to_address_zip: rep_attributes[:address_zip],
+        from_address_line1: user_attributes[:address_line1],
+        from_address_line2: user_attributes[:address_line2],
+        from_address_city: user_attributes[:address_city],
+        from_address_state: user_attributes[:address_state],
+        from_address_zip: user_attributes[:address_zip],
+        body: params[:body],
+        user_id: user_attributes[:user_id],
+        to_name: rep_attributes[:name],
+        from_name: user_attributes[:name]
+       }
+    end
 end
