f90408ce6f05e826a253c5b8c20140fc605718ad
Christina Delpone <cdelpone@gmail.com>
2022-01-09 17:55:47 -0700

    Test user dash welcomes user without an address

--- spec/features/users/show_spec.rb
+++ spec/features/users/show_spec.rb
@@ -1,17 +1,63 @@
 require 'rails_helper'
 
-describe 'users dashboard' do
+describe 'users dashboard', :vcr do
   before :each do
-    @user = User.create(email: 'bob@example.com', name: 'bob', street_address_1: '123 Main Street')
+    @user = User.create(email: 'prisonmike@theoffice.com', name: 'Michael Scott')
     @session = {user_id: @user.id, token: 'abcd', google_id: '12345'}
 
     allow_any_instance_of(ApplicationController).to receive(:session).and_return(@session)
+    stub_request(:get, "http://localhost:5000/api/v1/users/#{@user.id}").
+    with(
+      headers: {
+        'Accept'=>'*/*',
+        'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+        'User-Agent'=>'Faraday v1.8.0'
+        }).to_return(status: 200, body:
+          { data: {
+            id: @user.id,
+            type: "user",
+            attributes: {
+              email: @user.email,
+              name: @user.name,
+              google_id: @user.google_id,
+              address_line1: nil,
+              address_line2: nil,
+              address_city: nil,
+              address_state: nil,
+              address_zip: nil
+            }
+          }
+        }.to_json,
+        headers: {} )
+        stub_request(:get, "http://localhost:5000/api/v1/users/#{@user.id}/representatives").
+        with(
+          headers: {
+            'Accept'=>'*/*',
+            'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+            'User-Agent'=>'Faraday v1.8.0'
+            }).to_return(status: 200, body:
+              {
+                data: [
+                  {
+                    id: 0,
+                    type: "representative",
+                    attributes: {
+                      address_city: "Washington",
+                      address_line1: "1600 Pennsylvania Avenue Northwest",
+                      address_state: "DC",
+                      address_zip: "20500",
+                      name: "Joseph R. Biden"
+                    }
+                    }]}.to_json,
+                    headers: {})
+                    found_user = BackendService.get_user("#{@user.id}")
+                    found_rep = BackendService.representatives("#{@user.id}")
   end
 
   it 'should say hello' do
     visit '/dashboard'
 
-    expect(page).to have_content "Welcome, #{@user.name}"
+    expect(page).to have_content "Welcome, Michael Scott"
   end
 
   it 'should have a button to update addresses' do
