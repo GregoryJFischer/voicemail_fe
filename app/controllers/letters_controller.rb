d3aee270cd3290eda4a209b3d27cfa643414bf5f
Billy Frey <bfrey16@gmail.com>
2022-01-10 20:33:12 -0700

    refactor: create poro and facade for letters to slim the controller

--- app/controllers/letters_controller.rb
+++ app/controllers/letters_controller.rb
@@ -4,8 +4,7 @@ class LettersController < ApplicationController
   end
 
   def create
-     response = BackendService.post("letters", letter_params)
-     confirmation = BackendService.parse_response(response)
+     confirmation = LettersFacade.create_letter(params[:body], session[:user_id])
      if confirmation.has_key?(:message)
        flash[:error] = confirmation[:message]
      elsif confirmation[:data][:attributes][:send_date]
@@ -13,35 +12,6 @@ class LettersController < ApplicationController
        redirect_to "/dashboard"
      else
        flash[:error] = 'Error and error message not found'
-  end
-
+     end
+   end
 end
-  private
-    def letter_params
-      user_id = session[:user_id]
-      user_body = BackendService.get_user(user_id)
-      user_attributes = user_body[:data][:attributes]
-
-      rep_body = BackendService.representatives(user_id)
-
-      #This line needs to be updated once we have our reps page up and running
-      rep_attributes = rep_body[:data][11][:attributes]
-
-      {
-        to_address_line1: rep_attributes[:address_line1],
-        to_address_line2: rep_attributes[:address_line2],
-        to_address_city: rep_attributes[:address_city],
-        to_address_state: rep_attributes[:address_state],
-        to_address_zip: rep_attributes[:address_zip],
-        from_address_line1: user_attributes[:address_line1],
-        from_address_line2: user_attributes[:address_line2],
-        from_address_city: user_attributes[:address_city],
-        from_address_state: user_attributes[:address_state],
-        from_address_zip: user_attributes[:address_zip],
-        body: params[:body],
-        user_id: user_id,
-        to_name: rep_attributes[:name],
-        from_name: user_attributes[:name]
-       }
-    end
-  end
---
+++ app/poros/letter.rb
@@ -0,0 +1,39 @@
+class Letter
+  attr_reader :to_address_line1, :to_address_line2, :to_address_city, :to_address_state, :to_address_zip, :to_name,
+  :from_address_line1, :from_address_line2, :from_address_city, :from_address_state, :from_address_zip, :user_id, :from_name,
+  :body
+  def initialize(rep_attributes, user_attributes, body)
+    @to_address_line1 = rep_attributes[:attributes][:address_line1]
+    @to_address_line2 = rep_attributes[:attributes][:address_line2]
+    @to_address_city = rep_attributes[:attributes][:address_city]
+    @to_address_state = rep_attributes[:attributes][:address_state]
+    @to_address_zip = rep_attributes[:attributes][:address_zip]
+    @to_name = rep_attributes[:attributes][:name]
+    @from_address_line1 = user_attributes[:attributes][:address_line1]
+    @from_address_line2 = user_attributes[:attributes][:address_line2]
+    @from_address_city = user_attributes[:attributes][:address_city]
+    @from_address_state = user_attributes[:attributes][:address_state]
+    @from_address_zip = user_attributes[:attributes][:address_zip]
+    @user_id = user_attributes[:id]
+    @from_name = user_attributes[:attributes][:name]
+    @body = body
+  end
+  def attributes
+        {
+          to_address_line1: @to_address_line1,
+          to_address_line2: @to_address_line2,
+          to_address_city: @to_address_city,
+          to_address_state: @to_address_state,
+          to_address_zip: @to_address_zip,
+          from_address_line1: @from_address_line1,
+          from_address_line2: @from_address_line2,
+          from_address_city: @from_address_city,
+          from_address_state: @from_address_state,
+          from_address_zip: @from_address_zip,
+          body: @body,
+          user_id: @user_id,
+          to_name: @to_name,
+          from_name: @from_name
+         }
+  end
+end
