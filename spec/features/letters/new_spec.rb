848b1de5d1eceddf66f60f0c0419905c1585bee1
Billy Frey <bfrey16@gmail.com>
2022-01-10 21:19:40 -0700

    test: add new letter page test :: simple

--- spec/features/letters/new_spec.rb
+++ spec/features/letters/new_spec.rb
@@ -3,6 +3,10 @@ require 'rails_helper'
 describe 'letters new' do
   before :each do
   end
-  it 'can show a new letter' do 
+  it 'can show a new letter page' do
+    visit '/letters/new' do
+
+    expect(page).to have_content("Create New Letter")
+    end
   end
 end
