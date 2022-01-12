da4a63488888878b1f6b66fd54783e5de77d871b
Alex McConnell <alexmmcconnell@gmail.com>
2022-01-12 16:43:25 -0700

    Refactor - Adjust faraday to use Base URL for all requests

--- app/services/backend_service.rb
+++ app/services/backend_service.rb
@@ -41,8 +41,7 @@ class BackendService
     end
 
     def conn
-       Faraday.new(url: "https://voicemail-be.herokuapp.com")
-       # Faraday.new(url: "http://localhost:5000")
+      Faraday.new(url: ENV['BASE_URL'])
     end
   end
 end
