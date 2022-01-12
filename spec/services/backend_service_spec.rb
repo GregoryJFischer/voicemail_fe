7e82f048dee1c1d32d195c58633d7ee9ce35db0e
Christina Delpone <cdelpone@gmail.com>
2022-01-12 10:37:43 -0700

    Revert back

--- spec/services/backend_service_spec.rb
+++ spec/services/backend_service_spec.rb
@@ -7,30 +7,30 @@ describe BackendService do
     allow_any_instance_of(ApplicationController).to receive(:session).and_return(@session)
   end
 
-  it 'get an user', :vcr do
-    # stub_request(:get, "http://localhost:5000/api/v1/users/#{@user.id}").
-    # with(
-    #   headers: {
-    #     'Accept'=>'*/*',
-    #     'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-    #     'User-Agent'=>'Faraday v1.9.3'
-    #     }).to_return(status: 200, body:
-    #       { data: {
-    #         id: @user.id,
-    #         type: "user",
-    #         attributes: {
-    #           email: @user.email,
-    #           name: @user.name,
-    #           google_id: @user.google_id,
-    #           address_line1: nil,
-    #           address_line2: nil,
-    #           address_city: nil,
-    #           address_state: nil,
-    #           address_zip: nil
-    #         }
-    #       }
-    #     }.to_json,
-    #     headers: {} )
+  it 'get an user' do
+    stub_request(:get, "http://localhost:5000/api/v1/users/#{@user.id}").
+    with(
+      headers: {
+        'Accept'=>'*/*',
+        'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+        'User-Agent'=>'Faraday v1.9.3'
+        }).to_return(status: 200, body:
+          { data: {
+            id: @user.id,
+            type: "user",
+            attributes: {
+              email: @user.email,
+              name: @user.name,
+              google_id: @user.google_id,
+              address_line1: nil,
+              address_line2: nil,
+              address_city: nil,
+              address_state: nil,
+              address_zip: nil
+            }
+          }
+        }.to_json,
+        headers: {} )
 
     response = BackendService.get_user(@user.id)
     expect(response).to be_a Hash
