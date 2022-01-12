c31d9cc25a110ad5e90eef966e2ceafe16c5d587
Gregory Fischer <gwhoisj@gmail.com>
2022-01-11 20:42:02 -0500

    Feat: boostrap design

--- app/poros/representative.rb
+++ app/poros/representative.rb
@@ -1,13 +1,15 @@
 class Representative
   attr_reader :id,
               :name,
+              :title,
               :address_line1,
               :address_city,
               :address_state,
               :address_zip
 
   def initialize(rep)
-    @name = rep[:attributes][:name]
+    @name          = rep[:attributes][:name]
+    @title         = rep[:attributes][:title]
     @address_line1 = rep[:attributes][:address_line1]
     @address_city  = rep[:attributes][:address_city]
     @address_state = rep[:attributes][:address_state]
