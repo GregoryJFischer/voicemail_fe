8ca687ad1fa1c29b61f216472356b9894d377d1f
Alex McConnell <alexmmcconnell@gmail.com>
2022-01-12 11:04:00 -0700

    Test - Add happy and sad tests for filling out a new letter

--- spec/features/letters/new_spec.rb
+++ spec/features/letters/new_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 describe 'letters new' do
   before :each do
   end
+
   it 'can show a new letter page' do
     rep_attributes = { attributes: {
                                     :address_city=>"Denver",
@@ -11,11 +12,44 @@ describe 'letters new' do
                                     :address_zip=>"80203",
                                     :name=>"CO State Representative Alec Garnett"
                                   }
+                        }
+      visit new_letter_path(rep_attributes) do
+
+      expect(page).to have_content("Sending letter to")
+    end
+  end
+
+  it 'allows you to fill out and send a letter' do
+    rep_attributes = { attributes: {
+                                    :address_city=>"Denver",
+                                    :address_line1=>"200 East Colfax Avenue",
+                                    :address_state=>"CO",
+                                    :address_zip=>"80203",
+                                    :name=>"CO State Representative Alec Garnett"
+                                  }
                       }
-    visit new_letter_path(rep_attributes) do
+      visit new_letter_path(rep_attributes) do
 
+      fill_in :body, with: "Senator Michael Bennet, Please make GrubHub free. Your other constituent, Alex"
+      click_button "Submit"
 
-    expect(page).to have_content("Sending letter to")
+      expect(current_path).to eq("/letters")
+    end
+  end
+
+  it 'doesnt accept the letter if the letter isnt filled out' do
+    rep_attributes = { attributes: {
+                                    :address_city=>"Denver",
+                                    :address_line1=>"200 East Colfax Avenue",
+                                    :address_state=>"CO",
+                                    :address_zip=>"80203",
+                                    :name=>"CO State Representative Alec Garnett"
+                                  }
+                      }
+      visit new_letter_path(rep_attributes) do
+      click_button "Submit"
+
+      expect(current_path).to eq(new_letter_path(rep_attributes))
     end
   end
 end
