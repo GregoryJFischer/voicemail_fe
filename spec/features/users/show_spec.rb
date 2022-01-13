11df5e0bde227e23dbf85be5f81eb6c78faf29e1
Gregory Fischer <67120586+GregoryJFischer@users.noreply.github.com>
2022-01-13 11:10:23 -0500

    Update show_spec.rb
    Fix button name

--- spec/features/users/show_spec.rb
+++ spec/features/users/show_spec.rb
@@ -62,9 +62,9 @@ describe 'users dashboard' do
     it 'should have a button to add a return address' do
       visit '/dashboard'
 
-      expect(page).to have_button "Add your return address"
+      expect(page).to have_button "Add a valid return address to see your representatives"
 
-      click_button "Add your return address"
+      click_button "Add a valid return address to see your representatives"
 
       expect(current_path).to eq("/edit")
     end
