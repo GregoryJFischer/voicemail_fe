831ed03bb5c736e62681aae8dfb904bd72b6cbd1
Nathan Brown <nathan.brown263@gmail.com>
2022-01-22 11:38:43 -0700

    feat: First working redirect to Stripe payment

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -1,22 +1,28 @@
 class CheckoutController < ApplicationController
   def create
-    price = 99
-    quantity = 1
-  @session = Stripe::Checkout::Session.create({
-    payment_method_types: ['card'],
-    line_items: [{
-      # name: 'Letter to Your Representative',
-      price: "#{price}",
-      currency: 'usd',
-      quantity: "#{quantity}"
-    }],
-    mode: 'payment',
-    success_url: "#{ENV['BASE_URL']}/dashboard.html",
-    cancel_url: "#{ENV['BASE_URL']}/cancel.html"
-  })
+    letter = Stripe::Product.create(name: 'Letter')
 
-  respond_to do |format|
-    format.js
-  end
+    price = Stripe::Price.create(
+      {
+        product: "#{letter.id}",
+        unit_amount: 99,
+        currency: 'usd',
+      }
+    )
+
+    @session = Stripe::Checkout::Session.create({
+      payment_method_types: ['card'],
+      line_items: [{
+        price: "#{price.id}",
+        quantity: 1
+      }],
+      mode: 'payment',
+      success_url: "#{ENV['BASE_URL']}/dashboard.html",
+      cancel_url: "#{ENV['BASE_URL']}/letters/new.html"
+    })
+
+    respond_to do |format|
+      format.js
+    end
   end
 end
